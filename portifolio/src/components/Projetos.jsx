1bc19fbabf305889f790a12a3f6f46a480d392bc
Marcos Gabriel Paz <mgabrielpazdeoliveira@gmail.com>
2024-05-06 23:30:57 -0300

    atv

--- portifolio/src/components/Projetos.jsx
+++ portifolio/src/components/Projetos.jsx
@@ -7,19 +7,19 @@ export default function Projeto() {
             <h1>Projetos</h1>
             <article>
                 <h4>Cadastro</h4>
-                <p>https://github.com/Marcaogabriel/React/tree/main/atividade1</p>
+                <p>https://github.com/Marcaogabriel/React/tree/main/cadastro</p>
             </article>
             <article>
                 <h4>Catalogos de Filmes</h4>
                 <p>https://github.com/Marcaogabriel/React/tree/main/atividade2</p>
             </article>
             <article>
-                <h4>Jogo de cartas</h4>
-                <p>https://github.com/Marcaogabriel/React/tree/main/atividade3</p>
+                <h4>Farmacia</h4>
+                <p>https://github.com/Marcaogabriel/React/tree/main/farmacia</p>
             </article>
             <article>
-                <h4>Farmacia</h4>
-                <p>https://github.com/Marcaogabriel/React/tree/main/atividade3</p>
+                <h4>Jogo de cartas</h4>
+                <p>https://github.com/Marcaogabriel/React/tree/main/jogodecartas</p>
             </article>
         </section>
     </div>
